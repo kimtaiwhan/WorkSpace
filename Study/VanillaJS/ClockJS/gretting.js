ad350492c75a2b8254818d5e275ba6c35c64997d
Tae Hwan <kimtaiwhan@gmail.com>
2020-11-18 18:09:16 +0900

    update ClockJS

--- Study/VanillaJS/ClockJS/gretting.js
+++ Study/VanillaJS/ClockJS/gretting.js
@@ -1,6 +1,6 @@
-const form = document.querySelector(".js-form");
-const input = form.document.querySelector("input");
-const greeting = document.querySelector(".js-greetings");
+const form = document.querySelector(".js-form"),
+    input = form.document.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
 const User_LS = "currentUser";
 const SHOWING_CN = "showing";
 
