d66c111f5dcf11fc681117ee71edd7e9c93f8a7b
Tae Hwan <kimtaiwhan@gmail.com>
2021-01-21 04:59:50 +0900

    update

--- Study/BAEKJOON/Basic Math1/1193.py
+++ Study/BAEKJOON/Basic Math1/1193.py
@@ -1,40 +1,13 @@
-#1 1 2 3 2 1 1 2 3 4 5 4 3 2 1
-#15 - 9 6     3
-#6 - 3 3      2
-#
-#6 - 9 3
-#1 2 1 1 2 3 4 3 2 1 1 2 3 4 5
-
 X = int(input())
-UpNum = 1
-DownNum = 3
-n = 1
-
-#1개 3X2-1개 5X2-1개
-#(2n + 1)
-#(2n) X 2 - 1
-
-if X == 1:
-    molecule = 1
-    denominator = 1
+floor, start = 1, 1
+while start + floor <= X:
+    start += floor
+    floor += 1
+num = X - start
+x, y = num + 1, floor - num
+if floor % 2 == 0:
+    print('{}/{}'.format(x, y))
 else:
-    while(True):
-        if X > UpNum:
-            UpNum = UpNum + (2 * n + 1) * 2 - 1
-            n += 1
-        elif X == UpNum:
-            molecule = 1
-            break
-        else:
-            n -= 1
-            UpNum = UpNum - ((2 * n + 1) * 2 - 1)
-            temp = X - UpNum
-            print(temp)
-            print(2 * n + 1)
-            if temp > 2 * n + 1:
-                molecule = temp - (2 * n + 1)
-            else:
-                molecule = temp
-            break
+    print('{}/{}'.format(y, x))
 
-print(molecule)
+# 몰라서 보고했음
--- Study/BAEKJOON/Basic Math1/2775.py
+++ Study/BAEKJOON/Basic Math1/2775.py
@@ -1,19 +1,10 @@
-def sig(n):
-    if n == 1:
-        return 1
-    if n == 2:
-        return n + 1
-    return n * (n - 1) // 2 + sig(n - 1)
-
-for i in range(int(input(""))):
+for T in range(int(input(""))):
     k = int(input(""))
     n = int(input(""))
-    if n == 1:
-        num = 1
-    elif n == 2:
-        num = k + 2
-    else:
-        num = num + 
-#1 5 15 35 70 126 ..
-#1 4 10 20 35 56 84
-#1 3  6 10 15 21 28
+    f = [x for x in range(1, n + 1)]
+    for i in range(k):
+        for j in range(1, n):
+            f[j] += f[j-1]
+    print(f[-1])
+
+# 몰라서 보고했음
