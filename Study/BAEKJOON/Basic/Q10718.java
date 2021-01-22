9f76285d2cc807849f73e19d9b1431bf8da2fb79
Tae Hwan <kimtaiwhan@naver.com>
2021-01-22 23:46:17 +0900

    update

---
+++ Study/BAEKJOON/Basic/Q10718.java
@@ -0,0 +1,7 @@
+package WorkSpace.Study.BAEKJOON.Basic;
+
+public class Q10718 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군\n강한친구 대한육군");
+    }
+}
---
+++ Study/BAEKJOON/Basic/cnpz.java
@@ -0,0 +1,8 @@
+package WorkSpace.Study.BAEKJOON.Basic;
+
+public class cnpz {
+    public static void main(String[] args) {
+        Double f = 1.1;
+        System.out.println(f.getClass());
+    }
+}
